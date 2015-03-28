a340356822bd1e36b7993b0a12ed8119b7bbaa3d
God <gf@gfzj.us>
2015-03-28 02:25:26 -0400

    增加命令参数: ./ipac.js [port] [useCloudflareSSL?true/false]

--- ipac.js
+++ ipac.js
@@ -4,7 +4,7 @@ var http = require('http');
 var fs = require('fs');
 var ipac = {
 	port: process.argv.length>2&&parseInt(process.argv[2])?parseInt(process.argv[2]):55555,//服务器端口号
-	useCloudflareHttps: true, //可以根据自己的情况修改。是否使用cloudflare并强制使用https
+	useCloudflareHttps: process.argv.length>3&&process.argv[3]=="true"?true:false, //可以根据自己的情况修改。是否使用cloudflare并强制使用https
 	hostsStr: "",
 	lastUpdateTime: 0,
 	count: 0,
