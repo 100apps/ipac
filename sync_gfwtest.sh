8ea95601abebf5d12293f503183dcf301fe8f4aa
God <gf@gfzj.us>
2015-03-28 02:16:58 -0400

    修改crontab当前目录的bug

--- sync_gfwtest.sh
+++ sync_gfwtest.sh
@@ -1,5 +1,7 @@
 #/bin/bash
+cd "$(dirname "$0")"
 git fetch gfwtest.js
 wget http://autoproxy2pac.appspot.com/gfwtest.js -O gfwtest.js
 git commit gfwtest.js -m "update `date '+%Y-%m-%d %H:%M:%S'`"
 git push
+date >> .sync.log
